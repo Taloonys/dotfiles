b57a589fb1838f849ab966621b73af1f4d7be7c0
Taloonys <vadim-dev@vadimrevi11pc.>
2025-06-06 11:51:15 +0300

    feat: nvim & gitconfig

---
+++ nvim/lua/config/telescope.lua
@@ -0,0 +1,16 @@
+local builtin 	= require('telescope.builtin')
+
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+
+map('n', '<leader>pf', builtin.find_files, {}) 	-- main use
+map('n', '<C-p>', builtin.git_files, {})		-- browse git files
+map('n', '<leader>ps', function() 				-- Can find <input> inside project files
+	builtin.grep_string( { search = vim.fn.input("Grep > ") }); 
+end)
+
+-- IDK about this one
+-- map('n', '<leader>fg', builtin.live_grep, {})
+-- map('n', '<leader>fb', builtin.buffers, {})
+-- map('n', '<leader>fh', builtin.help_tags, {})
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt -- aka options
+
+-- lines
+opt.number 			= true                   
+opt.relativenumber 	= true          
+
+-- tabulation
+opt.shiftwidth 	= 4
+opt.tabstop 	= 4
