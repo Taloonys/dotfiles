f94cde9ea6d6d9b3ea9e20eadc3a680a412c6ca4
v.makrousov <v.makrousov@securitycode.ru>
2025-06-26 12:24:08 +0300

    chore: grabbed some stuff from main

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -4,18 +4,20 @@ local map = vim.keymap.set
 
 vim.g.mapleader = " "
 
-map('n', '<leader>pf', builtin.find_files, {}) 				-- main use
-map('n', '<C-p>', builtin.git_files, {})					-- browse git files
-map('n', '<leader>ps', function() 							-- Can find <input> inside project files
+map('n', '<leader>pf', builtin.find_files, {}) 	            -- main use
+map('n', '<C-p>', builtin.git_files, {})		            -- browse git files
+map('n', '<leader>ps', function() 				            -- Can find <input> inside project files
 	builtin.grep_string( { search = vim.fn.input("Grep > ") }); 
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})	-- find through manual
+
+Map("n", "<leader>fa", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>")   -- find all files (include hidden)
+Map("n", "<leader>fe", "<cmd> Telescope file_browser <CR>")
 
 -- Buffers overview
 map('n', '<leader>fg', builtin.live_grep, {})
 map('n', '<leader>fb', builtin.buffers, {})
 
--- TelescopeUI transparency
 vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,6 +1,52 @@
-local map 	= vim.keymap.set
+vim.g.Mapleader = " "       -- <leader> is now whitespace
 
-vim.g.mapleader = " "
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }      -- disable recursive Mappings + no feedback about command completion
+    if opts then
+        options = vim.tbl_extend("force", options, opts)   -- rewrite options if presented
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
 
--- Defaults edit
-map('n', '<leader>pv', vim.cmd.Ex)
+-- File explorer
+Map('n', '<leader>pv', vim.cmd.Ex)
+
+-- System buffer
+Map({'n', 'v'}, '<leader>y', '"+y') -- selected
+Map({'n', 'v'}, '<leader>Y', '"+Y') -- whole line
+
+Map({'n', 'v'}, '<leader>d', '"+d') -- selected
+Map({'n', 'v'}, '<leader>D', '"+D') -- whole line
+
+Map('n', '<leader>p', '"+p')  -- after cursor (Somehow it's slow*)
+Map('n', '<leader>P', '"+P')  -- before cursor
+
+-- Split navigation
+Map("n", "<C-h>", "<C-w>h")
+Map("n", "<C-j>", "<C-w>j")
+Map("n", "<C-k>", "<C-w>k")
+Map("n", "<C-l>", "<C-w>l")
+
+Map("n", "<C-Up>", ":resize -2<CR>")
+Map("n", "<C-Down>", ":resize +2<CR>")
+Map("n", "<C-Left>", ":vertical resize -2<CR>")
+Map("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Visual mode
+Map("v", "J", ":m '>+1<CR>gv=gv") -- drag selected text 1 line up
+Map("v", "K", ":m '<-2<CR>gv=gv") -- drag selected text 1 line down
+
+Map("v", "<", "<gv")  -- drag 1 tab left (and don't lose selection!)
+Map("v", ">", ">gv")  -- drag 1 tab right (and don't lose selection!)
+
+-- Tabs
+Map("n", "<TAB>", ":bn<CR>")
+Map("n", "<S-TAB>", ":bp<CR>")
+Map("n", "<leader>bd", ":bd<CR>") 
+
+-- Basic stuff enchancements
+Map("n", "J", "mzJ`z")          -- concatenate lines, BUT keeps cursor position
+Map("n", "<C-d>", "<C-d>zz")    -- scroll down AND center cursor pos on screen
+Map("n", "<C-u>", "<C-u>zz")    -- scroll up AND center cursor pos on screen
+Map("n", "n", "nzzzv")          -- find next occurence AND center cursor + open fold
+Map("n", "N", "Nzzzv")          -- find previous occurence AND center cursor + open fold
