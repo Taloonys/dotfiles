b33ff2cea472c48ea8161782ec35ea94b75a4206
v.makrousov <v.makrousov@securitycode.ru>
2025-07-11 14:12:41 +0300

    chore(toggleterm): fixed ui

--- nvim/lua/config/terminal.lua
+++ nvim/lua/config/terminal.lua
@@ -1,18 +1,18 @@
 require("toggleterm").setup({
-	-- [[
-	-- Settings are applied only by wsl (for example) relaunching...
-	-- ]]
-	open_mapping = [[<c-\>]],
-	shell = "powershell",
-	autochdir = false,
-	direction = "float",
-	float_opts = {
-		border = "curved", -- also possible "single", "double", "shadow"
-		width = function() return math.floor(vim.o.columns * 0.8) end,
-		height = function() return math.floor(vim.o.lines * 0.8) end,
-		winblend = 3, -- transperancy (0-100)
-	},
-  shade_terminals = false, -- idk, but required for float
-  persist_size = false,
-  persist_mode = false,
+    -- [[
+    -- Settings are applied only by wsl (for example) relaunching...
+    -- ]]
+    open_mapping = [[<c-\>]],
+    shell = "nu",
+    autochdir = false,
+    direction = "float",
+    float_opts = {
+        border = "rounded", -- also possible "single", "double", "shadow"
+        width = function() return math.floor(vim.o.columns * 0.8) end,
+        height = function() return math.floor(vim.o.lines * 0.8) end,
+        winblend = 0,        -- transperancy (0-100)
+    },
+    shade_terminals = false, -- idk, but required for float
+    persist_size = false,
+    persist_mode = false,
 })
